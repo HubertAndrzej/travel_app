ac4afb2cf60a2f87c4475faa980923fc8e28dcdd
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2023-12-22 03:06:16 +0100

    Added New Booking Screen

--- lib/screens/new_booking_screen.dart
+++ lib/screens/new_booking_screen.dart
@@ -12,6 +12,115 @@ class NewBookingScreen extends StatefulWidget {
 class _NewBookingScreenState extends State<NewBookingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('New');
+    return Column(
+      children: [
+        const SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Cześć,\nNastępna podróż?',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSecondary,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        Card(
+          elevation: 5,
+          margin: const EdgeInsets.all(16),
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/flight.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 200,
+              ),
+              Positioned(
+                bottom: 16,
+                right: 16,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          'Loty',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 32,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 30),
+        Card(
+          elevation: 5,
+          margin: const EdgeInsets.all(16),
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/hotel.jpeg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 200,
+              ),
+              Positioned(
+                bottom: 16,
+                right: 16,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          'Hotele',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 32,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
